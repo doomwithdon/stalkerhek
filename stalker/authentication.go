d6214a81f957cb98ba0cef576983fa4215b5c869
CrazeeGhost <21267479+CrazeeGhost@users.noreply.github.com>
2023-12-21 13:57:08 -0500

    Fixed condition check for deviceIDs

--- stalker/authentication.go
+++ stalker/authentication.go
@@ -66,7 +66,7 @@ func (p *Portal) authenticate() (err error) {
 		content, err := p.httpRequest(p.Location + "?type=stb&action=do_auth&login=" + p.Username + "&password=" + p.Password + "&device_id=" + p.DeviceID + "&device_id2=" + p.DeviceID2 + "&JsHttpRequest=1-xml")
 	}
 	
-	else if p.Username != "" && p.Password != "" {
+	else if p.DeviceID != "" && p.DeviceID2 != "" {
 		log.Println("Authenticating with DeviceId and DeviceId2")
 		content, err := p.httpRequest(p.Location + "?type=stb&action=get_profile&JsHttpRequest=1-xml&hd=1&sn=" + p.SerialNumber + "&stb_type=" + p.Model + "&device_id=" + p.DeviceID + "&device_id2=" + p.DeviceID2 + "auth_second_step=1")
 	}
